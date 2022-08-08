7a321acde46bd9b85443096011488d6682fc2ab5
Zzl85199 <jonathan40507@gmail.com>
2022-08-08 14:30:28 +0800

    Initial Commit

---
+++ swiftpracapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftpracapp
+//
+//  Created by 張哲綸 on 2022/8/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftpracapp/swiftpracappApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftpracappApp.swift
+//  swiftpracapp
+//
+//  Created by 張哲綸 on 2022/8/8.
+//
+
+import SwiftUI
+
+@main
+struct swiftpracappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
